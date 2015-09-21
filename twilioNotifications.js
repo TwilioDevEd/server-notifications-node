aca0c3bc3de22316a83eb1f6655d548da13a05a7
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-09-21 13:59:29 -0500

    Add message format

--- twilioNotifications.js
+++ twilioNotifications.js
@@ -1,16 +1,24 @@
+// jshint multistr:true
+
 var twilioClient = require('./twilioClient');
 var fs = require('fs');
 var _ = require('underscore');
 
+var formatMessage = function(errorToReport) {
+  return '[This is a test] ALERT! It appears the server is \
+having issues. Exception: ' + errorToReport + '. Go to: http://newrelic.com \
+for more details.';
+};
+
 module.exports.notifyOnError = function(appError, request, response, next) {
-  fs.readFile('./config/administrators.json', 'utf8', function(readError, fileData) {
-    if (readError) {
-      console.error('Could not read administrators file: ' + readError.toString());
+  fs.readFile('./config/administrators.json', 'utf8', function(readErr, data) {
+    if (readErr) {
+      console.error('Could not read administrators file: ' + readErr.toString());
     } else {
-      var admins = JSON.parse(fileData);
+      var admins = JSON.parse(data);
 
       _.each(admins, function(admin) {
-        twilioClient.sendSms(admin.phoneNumber, appError.message);
+        twilioClient.sendSms(admin.phoneNumber, formatMessage(appError.message));
       });
     }
   });
