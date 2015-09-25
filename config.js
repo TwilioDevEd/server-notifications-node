ebbbd24af17bd270a61a37d16bc0567701671814
Kevin Whinnery <kevin.whinnery@gmail.com>
2015-09-25 10:47:00 -0500

    clean up default check

--- config.js
+++ config.js
@@ -24,19 +24,14 @@ cfg.accountSid = process.env.TWILIO_ACCOUNT_SID;
 cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
 cfg.sendingNumber = process.env.TWILIO_NUMBER;
 
-var configured =
-      [cfg.accountSid, cfg.authToken, cfg.sendingNumber]
-      .every(function(configValue) {
-        if (configValue) {
-          return true;
-        } else {
-          return false;
-        }
-      });
+var requiredConfig = [cfg.accountSid, cfg.authToken, cfg.sendingNumber];
+var isConfigured = requiredConfig.every(function(configValue) {
+  return configValue || false;
+});
 
-if (!configured) {
+if (!isConfigured) {
   var errorMessage =
-        'TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, and TWILIO_NUMBER must be set';
+    'TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, and TWILIO_NUMBER must be set.';
 
   throw new Error(errorMessage);
 }
