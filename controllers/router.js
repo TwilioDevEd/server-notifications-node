3a5e6d1e48461a5e59f71eee863791239e2ca34f
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-11-02 16:09:57 -0400

    setup my first test using creds

--- controllers/router.js
+++ controllers/router.js
@@ -1,6 +1,6 @@
 // Map routes to controller functions
 module.exports = function(router) {
-  router.get('/error', function(req, resp) {
-    throw new Error('Derp. An error occurred.');
-  });
-};
+	router.get('/error', function(req, resp) {
+		throw new Error('Derp. An error occurred.')
+	})
+}
